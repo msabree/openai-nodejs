0b9c69b005b9fc5f14ecdc92246146733ead8a59
Makeen Sabree <makeen.sabree@gmail.com>
2024-01-05 22:06:17 -0500

    Added route to get github commit info.

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,20 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import OpenAI from "openai";
-import { Octokit } from "octokit";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+import { router as github } from "./routes/github";
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3003;
+const port = process.env.PORT || 3002;
 
-// open ai test
-const openai = new OpenAI();
+app.use(cors())
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 
-// github integration test
-const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN ?? '' });
-
-async function main() {
-  const {
-    data: { login },
-  } = await octokit.rest.users.getAuthenticated();
-  console.log("Hello, %s", login);
-
-  const response = await octokit.rest.repos.listCommits({
-    owner: login,
-    repo: "chess-app"
-  })
-
-  const commits = [];
-  for(let i = 0; i < response.data.length; i++) {
-    const message = response.data[i].commit.message;
-    const timestamp = response?.data[i]?.commit?.author?.date ?? new Date().toString();
-    commits.push({ message, timestamp });
-  }
-
-  const openAIResponse = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        "role": "system",
-        "content": " Summarize work in 2-3 bullet points\n\n"
-      },
-      {
-        "role": "user",
-        "content": JSON.stringify(commits)
-      },
-    ],
-    temperature: 1,
-    max_tokens: 256,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-  })
-
-  console.log(JSON.stringify(openAIResponse));
-}
-
-main();
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+app.use('/github', github)
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
---
+++ src/routes/github.ts
@@ -0,0 +1,61 @@
+import express from "express";
+import OpenAI from "openai";
+import { Octokit } from "octokit";
+
+const router = express.Router();
+
+router.get('/commits', async (req, res) => {
+    const { token } = req.query;
+
+    if (!token) {
+        res.status(400).send('No token provided');
+        return;
+    }
+
+    // open ai test
+    const openai = new OpenAI();
+
+    // github integration test
+    const octokit = new Octokit({ auth: token ?? '' });
+
+    const {
+        data: { login },
+      } = await octokit.rest.users.getAuthenticated();
+      console.log("Hello, %s", login);
+      
+      // the token doesn't have access to private repos...
+      const response = await octokit.rest.repos.listCommits({
+        owner: login,
+        repo: "standup-gpt"
+      })
+    
+      const commits = [];
+      for(let i = 0; i < response.data.length; i++) {
+        const message = response.data[i].commit.message;
+        const timestamp = response?.data[i]?.commit?.author?.date ?? new Date().toString();
+        commits.push({ message, timestamp });
+      }
+    
+      const openAIResponse = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            "role": "system",
+            "content": " Summarize work in 2-3 bullet points. Make it sound like a human wrote it."
+          },
+          {
+            "role": "user",
+            "content": JSON.stringify(commits)
+          },
+        ],
+        temperature: 1,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      })
+
+    res.send(openAIResponse);
+})
+
+export { router };
